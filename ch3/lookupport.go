ccd3ef9f3d96d7e4a019d9235dbfa2dbdd3dd4ac
igorakimy <igorakimy@gmail.com>
2023-07-07 09:42:21 +0300

    add ch3 - lookupport

---
+++ ch3/lookupport.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"os"
+)
+
+func main() {
+	if len(os.Args) != 3 {
+		log.Fatalf("Usage: %s network-type service\n", os.Args[0])
+	}
+	networkType := os.Args[1]
+	service := os.Args[2]
+	// Поиск и получение порта, на котором работает указанная служба в указанной сети
+	port, err := net.LookupPort(networkType, service)
+	if err != nil {
+		log.Fatalln("Error:", err.Error())
+	}
+	fmt.Println("Service port", port)
+}

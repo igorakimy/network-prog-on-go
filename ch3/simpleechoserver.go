6632f67726b490923b474f131f5363eb29e84580
igorakimy <igorakimy@gmail.com>
2023-07-08 07:55:17 +0300

    add ch3 - simpleechoserver

---
+++ ch3/simpleechoserver.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+)
+
+func main() {
+	service := ":1201"
+	tcpAddr, err := net.ResolveTCPAddr("tcp", service)
+	checkError(err)
+	listener, err := net.ListenTCP("tcp", tcpAddr)
+	checkError(err)
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			continue
+		}
+		handleClient(conn)
+		conn.Close()
+	}
+}
+
+func handleClient(conn net.Conn) {
+	var buf [512]byte
+	for {
+		n, err := conn.Read(buf[0:])
+		checkError(err)
+		fmt.Println(string(buf[0:]))
+		_, err = conn.Write(buf[0:n])
+		checkError(err)
+	}
+}
+
+func checkError(err error) {
+	if err != nil {
+		log.Fatalf("Fatal error: %s\n", err.Error())
+	}
+}

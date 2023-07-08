d5bf33851748983c87fafb4349ef1f84a5bf765c
igorakimy <igorakimy@gmail.com>
2023-07-08 13:37:29 +0300

    add ch3 - udpdaytimeclient

---
+++ ch3/udpdaytimeclient.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"os"
+)
+
+func main() {
+	if len(os.Args) != 2 {
+		log.Fatalf("Usage: %s host:port", os.Args[0])
+		
+	}
+	service := os.Args[1]
+	udpAddr, err := net.ResolveUDPAddr("udp", service)
+	checkError(err)
+	conn, err := net.DialUDP("udp", nil, udpAddr)
+	checkError(err)
+	_, err = conn.Write([]byte("anything"))
+	checkError(err)
+	var buf [512]byte
+	n, err := conn.Read(buf[0:])
+	checkError(err)
+	fmt.Println(string(buf[0:n]))
+}
+
+func checkError(err error) {
+	if err != nil {
+		log.Fatalf("Fatal error: %s", err.Error())
+	}
+}

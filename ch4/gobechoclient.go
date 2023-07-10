171e784480811fbb013e50b9231fd19ddc676030
igorakimy <igorakimy@gmail.com>
2023-07-10 19:15:10 +0300

    add ch4 - gobechoclient

---
+++ ch4/gobechoclient.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"encoding/gob"
+	"fmt"
+	"log"
+	"net"
+	"os"
+)
+
+type Person struct {
+	Name  Name
+	Email []Email
+}
+
+type Name struct {
+	Family   string
+	Personal string
+}
+
+type Email struct {
+	Kind    string
+	Address string
+}
+
+func main() {
+	if len(os.Args) != 2 {
+		log.Fatalln("Usage:", os.Args[0], "host:port")
+	}
+	person := Person{
+		Name: Name{Family: "Newmarch", Personal: "Jan"},
+		Email: []Email{
+			{Kind: "home", Address: "jan@newmarch.name"},
+			{Kind: "work", Address: "j.newmarch@boxhill.edu.au"},
+		},
+	}
+	service := os.Args[1]
+
+	conn, err := net.Dial("tcp", service)
+	checkError(err)
+	encoder := gob.NewEncoder(conn)
+	decoder := gob.NewDecoder(conn)
+	for n := 0; n < 10; n++ {
+		encoder.Encode(person)
+
+		var newPerson Person
+		decoder.Decode(&newPerson)
+		fmt.Println(newPerson)
+	}
+}
+
+func checkError(err error) {
+	if err != nil {
+		log.Fatalln("Fatal error", err.Error())
+	}
+}

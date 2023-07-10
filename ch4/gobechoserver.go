b2944174e0b502dbd3494233499be7491c351da7
igorakimy <igorakimy@gmail.com>
2023-07-10 19:16:06 +0300

    add ch4 - gobechoserver

---
+++ ch4/gobechoserver.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"encoding/gob"
+	"fmt"
+	"log"
+	"net"
+)
+
+type Person struct {
+	Name  Name
+	Email []Email
+}
+
+type Name struct {
+	Family   string
+	Personal string
+}
+
+type Email struct {
+	Kind    string
+	Address string
+}
+
+func main() {
+	service := "0.0.0.0:1200"
+	tcpAddr, err := net.ResolveTCPAddr("tcp", service)
+	checkError(err)
+	listener, err := net.ListenTCP("tcp", tcpAddr)
+	checkError(err)
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			continue
+		}
+		encoder := gob.NewEncoder(conn)
+		decoder := gob.NewDecoder(conn)
+		for n := 0; n < 10; n++ {
+			var person Person
+			decoder.Decode(&person)
+			fmt.Println(person)
+			encoder.Encode(person)
+		}
+	}
+}
+
+func checkError(err error) {
+	if err != nil {
+		log.Fatalln("Fatal error", err.Error())
+	}
+}

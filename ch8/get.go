a705fb4be9bddbfe845f0106268949f2985d063e
igorakimy <igorakimy@gmail.com>
2023-07-16 22:58:29 +0300

    add ch8 - get

---
+++ ch8/get.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"net/http/httputil"
+	"os"
+	"strings"
+)
+
+func main() {
+	if len(os.Args) != 2 {
+		log.Fatalln("Usage:", os.Args[0], "host:port")
+	}
+	url := os.Args[1]
+	response, err := http.Get(url)
+	checkError(err)
+	if response.StatusCode != http.StatusOK {
+		log.Fatalln(response.StatusCode)
+	}
+	fmt.Println("The response header is")
+	b, _ := httputil.DumpResponse(response, false)
+	fmt.Println(string(b))
+	contentTypes := response.Header["Content-Type"]
+	if !acceptableCharset(contentTypes) {
+		log.Fatalln("Cannot handle", contentTypes)
+	}
+	fmt.Println("The response body is")
+	var buf [512]byte
+	reader := response.Body
+	for {
+		n, err := reader.Read(buf[0:])
+		if err != nil {
+			if err == io.EOF {
+				fmt.Print(string(buf[0:]))
+				reader.Close()
+				break
+			}
+			checkError(err)
+		}
+		fmt.Print(string(buf[0:n]))
+	}
+}
+
+func acceptableCharset(contentTypes []string) bool {
+	// Каждый тип похож на [text/html; charset=utf-8],
+	// но нам нужна только utf-8
+	for _, cType := range contentTypes {
+		if strings.Index(cType, "utf-8") != -1 {
+			return true
+		}
+	}
+	return false
+}
+
+func checkError(err error) {
+	if err != nil {
+		log.Fatalln(err)
+	}
+}

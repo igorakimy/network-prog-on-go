dfb7059f51dbde054aabeb44da52ba674b331a37
igorakimy <igorakimy@gmail.com>
2023-07-16 22:05:01 +0300

    add ch8 - head

---
+++ ch8/head.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	if len(os.Args) != 2 {
+		log.Fatalln("Usage:", os.Args[0], "host:port")
+	}
+	url := os.Args[1]
+	response, err := http.Head(url)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	fmt.Println(response.Status)
+	for k, v := range response.Header {
+		fmt.Println(k+":", v)
+	}
+}
